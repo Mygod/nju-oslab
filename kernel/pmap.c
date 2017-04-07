a4a0037b72532e586778017f3689015cc1b4db98
Mygod <contact-git@mygod.be>
2017-04-07 22:57:33 +0800

    Refine pmap definitions

--- kernel/pmap.c
+++ kernel/pmap.c
@@ -1,11 +1,11 @@
 #include "assert.h"
 #include "memlayout.h"
 #include "pmap.h"
-#include "x86.h"
 
-#define PMAP_OFFSET 0x1000000 // need to avoid possibly hardware mapped region: 0xF00000 - 0xFFFFFF
-#define MAX_MEM     0x8000000 // QEMU default: 128MB
-#define PMAP_SIZE   (MAX_MEM - PMAP_OFFSET)
+#define PMAP_OFFSET   0x1000000 // need to avoid possibly hardware mapped region: 0xF00000 - 0xFFFFFF
+#define MAX_MEM       0x8000000 // QEMU default: 128MB
+#define PMAP_SIZE     (MAX_MEM - PMAP_OFFSET)
+#define PGTABLE_SIZE  (PMAP_SIZE >> PDXSHIFT)
 
 /*
  * IMPORTANT: Answer to question in lab2 handouts
@@ -19,10 +19,10 @@
 extern pde_t entry_pgdir[NPDENTRIES];
 static uint32_t allocated;
 __attribute__((__aligned__(PGSIZE)))
-static pte_t user_pgtable[PMAP_SIZE >> PDXSHIFT][NPTENTRIES];
+static pte_t user_pgtable[PGTABLE_SIZE][NPTENTRIES];
 
 void pmap_alloc(uint32_t addr, bool user) {
-  assert(allocated << PTSHIFT < PMAP_SIZE);
+  assert(allocated < PGTABLE_SIZE);
   addr >>= PTSHIFT;
   uint32_t flags = PTE_P | PTE_W;
   if (user) flags |= PTE_U;
@@ -30,6 +30,6 @@ void pmap_alloc(uint32_t addr, bool user) {
     user_pgtable[allocated][i] = (pte_t) ((PMAP_OFFSET + (allocated << PTSHIFT)) | (i << 12) | flags);
   entry_pgdir[addr] = (pde_t) (((uintptr_t) &user_pgtable[allocated] - KERNBASE) | flags);
   printk("pmap: Allocated 4MB at 0x%x to 0x%x, used %d/%d\n", addr << PTSHIFT, PMAP_OFFSET + (allocated << PTSHIFT),
-         allocated + 1, PMAP_SIZE >> PTSHIFT);
+         allocated + 1, PGTABLE_SIZE);
   ++allocated;
 }
