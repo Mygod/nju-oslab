2c3d1e7e0d78d55154d6bbcde1e51374625852ad
Mygod <contact-git@mygod.be>
2017-04-11 00:13:05 +0800

    Remove padding variables to simplify gdb output

--- include/trap.h
+++ include/trap.h
@@ -58,20 +58,20 @@ struct PushRegs {
 struct Trapframe {
   struct PushRegs tf_regs;
   uint16_t tf_es;
-  uint16_t tf_padding1;
+  uint16_t : 16;
   uint16_t tf_ds;
-  uint16_t tf_padding2;
+  uint16_t : 16;
   uint32_t tf_trapno;
   /* below here defined by x86 hardware */
   uint32_t tf_err;
   uintptr_t tf_eip;
   uint16_t tf_cs;
-  uint16_t tf_padding3;
+  uint16_t : 16;
   uint32_t tf_eflags;
   /* below here only when crossing rings, such as from user to kernel */
   uintptr_t tf_esp;
   uint16_t tf_ss;
-  uint16_t tf_padding4;
+  uint16_t : 16;
 } __attribute__((packed));
 
 struct UTrapframe {
